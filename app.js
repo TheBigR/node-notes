112698348715e2de1dabb987e22dd2db27a52424
TheBigR <nevoroy@gmail.com>
2021-03-09 13:49:55 +0200

    yarg added

--- app.js
+++ app.js
@@ -1,4 +1,37 @@
-const getNotes = require('./notes')
+const yargs = require('yargs')
 
-const notes = getNotes()
-console.log(notes)
+yargs.version('1.0.1')
+
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log('Adding a new note')
+  },
+})
+
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: () => {
+    console.log('Removing a note')
+  },
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log('Reading a note')
+  },
+})
+
+yargs.command({
+  command: 'list',
+  describe: 'List all notes',
+  handler: () => {
+    console.log('List all notes')
+  },
+})
+
+console.log(yargs.argv)
